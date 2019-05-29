e739aef92b04b6c5a6633657a42bdb9f1804cffe
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2019-05-29 11:15:19 -0700

    Fixing tsk pool

--- modules/libraries/standard/common/test/iot_tests_taskpool.c
+++ modules/libraries/standard/common/test/iot_tests_taskpool.c
@@ -560,7 +560,7 @@ TEST( Common_Unit_Task_Pool, CreateDestroyRecycleRecyclableJobError )
 {
     IotTaskPool_t taskPool = IOT_TASKPOOL_INITIALIZER;
     const IotTaskPoolInfo_t tpInfo = { .minThreads = 2, .maxThreads = 3, .stackSize = IOT_THREAD_DEFAULT_STACK_SIZE, .priority = IOT_THREAD_DEFAULT_PRIORITY };
-
+IotTaskPoolJobStatus_t statusAtCancellation = IOT_TASKPOOL_STATUS_READY;
     TEST_ASSERT( IotTaskPool_Create( &tpInfo, &taskPool ) == IOT_TASKPOOL_SUCCESS );
 
     if( TEST_PROTECT() )
@@ -573,6 +573,8 @@ TEST( Common_Unit_Task_Pool, CreateDestroyRecycleRecyclableJobError )
             TEST_ASSERT( IotTaskPool_CreateRecyclableJob( taskPool, &BlankExecution, NULL, &pJob ) == IOT_TASKPOOL_SUCCESS );
             /* Schedule deferred, then try to destroy it. */
             TEST_ASSERT( IotTaskPool_ScheduleDeferred( taskPool, pJob, ONE_HOUR_FROM_NOW_MS ) == IOT_TASKPOOL_SUCCESS );
+
+            error = IotTaskPool_TryCancel( taskPool, pJob, &statusAtCancellation );
             TEST_ASSERT( IotTaskPool_DestroyRecyclableJob( taskPool, pJob ) == IOT_TASKPOOL_SUCCESS );
         }
     }
